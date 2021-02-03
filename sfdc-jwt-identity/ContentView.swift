9f3697d9d7556cbbefa76f787c6d94a134b2737c
arupsarkar <sarkar.arup@gmail.com>
2021-02-02 20:57:39 -0500

    Initial Commit

---
+++ sfdc-jwt-identity/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  sfdc-jwt-identity
+//
+//  Created by Arup Sarkar on 2/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ sfdc-jwt-identity/sfdc_jwt_identityApp.swift
@@ -0,0 +1,17 @@
+//
+//  sfdc_jwt_identityApp.swift
+//  sfdc-jwt-identity
+//
+//  Created by Arup Sarkar on 2/2/21.
+//
+
+import SwiftUI
+
+@main
+struct sfdc_jwt_identityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
